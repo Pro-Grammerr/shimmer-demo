4c75ed9620f6825c488ad188c55394c3f3e2a1eb
arunsri7 <arun.i1997@gmail.com>
2020-06-15 21:57:32 +0530

    implemented shimmer [initial commit]

---
+++ app/src/main/java/com/example/shimmer_demo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.shimmer_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private ShimmerFrameLayout mShimmerViewContainer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mShimmerViewContainer.startShimmerAnimation();
+    }
+
+    @Override
+    protected void onPause() {
+        mShimmerViewContainer.stopShimmerAnimation();
+        super.onPause();
+    }
+}
